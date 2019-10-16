62b5c9b76a162a9881d7ed69aba96f48477c4a9a
Jonas Wolfram <jonas-wolfram@web.de>
2019-10-16 21:25:35 +0200

    Added gui for the insertion of projects in the database

--- app.js
+++ app.js
@@ -11,34 +11,68 @@ const server = http.createServer(app);
 const dbUrl = "mongodb://localhost:27017";
 const client = new mongoDb.MongoClient(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true });
 const database = "myportfolio";
-const collection = "projects";
+const collectionProjects = "projects";
+const collectionSlides = "slides"
 
-let col;
+let colProjects;
+let colSlides;
 
 //Initialization
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 client.connect((err) => {
     if (err) throw err;
     console.log(`Successfully connected to mongoDb with URL ${dbUrl}`);
 
     const db = client.db(database);
-    col = db.collection(collection);
+    colProjects = db.collection(collectionProjects);
+    colSlides = db.collection(collectionSlides)
     client.close();
 })
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-app.get("/projects/all", (req, res) => {
-    col.find().toArray((err, result) => {
+app.get("/projects", (req, res) => {
+    colProjects.find().toArray((err, result) => {
         if (err) throw err;
         console.log(`Fetched ${result.length} stored projects from the database`)
+        res.send(result)
+    })
+})
+
+app.post("/projects/:projectName", (req, res) => {
+    //: is used to to bind the value which stands behind the slash in the url request to the property defined here and is accessible via req.params 
+    //?propertyName=value defines the property in the url request itself and is accessible via req.query
+    colProjects.findOne({ "projectName": req.body.projectName }, (err, result) => {
+        if (err) throw err;
+        if (!result) {
+            colProjects.insertOne(req.body)
+                .then((result) => {
+                    console.log("Successfully inserted a document into the collection", result);
+                    //TODO Why is the inserted document stored inside an array?
+                    res.send({"_id": result.insertedId});
+                })
+                .catch((error) => {
+                    console.log("An error occurred trying to insert a document into the collection", error);
+                })
+        }
+        else {
+            console.log("Project with name " + req.body.projectName + " has already been inserted into the collection");
+        }
+    })
+})
+
+app.get("/slides", (req, res) => {
+    colSlides.find().toArray((err, result) => {
+        if (err) throw err;
+        console.log(`Fetched ${result.length} stored slides from the database`)
         res.send(result)
     })
 })
