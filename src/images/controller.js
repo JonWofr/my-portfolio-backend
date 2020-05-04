83354246f1853df30f8ed6051999a42b92278ffd
Jonas Wolfram <jonas-wolfram@web.de>
2020-05-04 17:58:00 +0200

    Properly forwarding Content-Type on image upload to AWS

--- src/images/controller.js
+++ src/images/controller.js
@@ -8,14 +8,15 @@ const imageminSvgo = require("imagemin-svgo");
 
 
 exports.uploadImage = async (req, res) => {
-    const { fileName, fileType } = req.query;
-    
-    if (!isSupportingFiletype(fileType)) {
-        console.warn(`Filetype ${fileType} is not supported.`);
-        return res.status(400).send(`Filetype ${fileType} is not supported.`);
+    const { filename: fileName } = req.query;
+    const contentType = req.get("Content-Type");
+
+    if (!isSupportingContentType(contentType)) {
+        console.warn(`Content-Type ${contentType} is not supported.`);
+        return res.status(400).send(`Content-Type ${contentType} is not supported.`);
     }
 
-    console.info(`About to save new image with name ${fileName} and type ${fileType}`);
+    console.info(`About to save new image with name ${fileName} and type ${contentType}`);
 
     try {
         let buffer = await readStream(req);
@@ -26,7 +27,7 @@ exports.uploadImage = async (req, res) => {
             filePath = await storeImageLocally(fileName, buffer)
         }
         else if (process.env.NODE_ENV === "production") {
-            filePath = await storeImageRemotely(fileName, buffer);
+            filePath = await storeImageRemotely(fileName, contentType, buffer);
         }
         else {
             console.warn(`Environment ${process.env.NODE_ENV} is not known. Image cannot be safed.`);
@@ -43,57 +44,62 @@ exports.uploadImage = async (req, res) => {
     }
 }
 
-const isSupportingFiletype = (filetype) => filetype === "image/jpeg" || filetype === "image/png" || filetype === "image/svg xml";
+const isSupportingContentType = (contentType) => {
+    return contentType === "image/jpeg" || contentType === "image/png" || contentType === "image/svg+xml";
+}
 
-const readStream = (readableStream) => new Promise((resolve, reject) => {
-    let chunks = [];
-    readableStream.on("error", err => reject(err));
-    readableStream.on("end", () => resolve(Buffer.concat(chunks)));
-    readableStream.on("readable", () => {
-        while (null !== (chunk = readableStream.read())) {
-            chunks.push(chunk);
-        }
-    });
-}) 
+const readStream = (readableStream) => {
+    return new Promise((resolve, reject) => {
+        let chunks = [];
+        readableStream.on("error", err => reject(err));
+        readableStream.on("end", () => resolve(Buffer.concat(chunks)));
+        readableStream.on("readable", () => {
+            while (null !== (chunk = readableStream.read())) {
+                chunks.push(chunk);
+            }
+        });
+    })
+}
 
-
-const compressImage = async (buffer) => await imagemin.buffer(buffer, {
+const compressImage = async (buffer) => {
+    return await imagemin.buffer(buffer, {
         plugins: [
             imageminMozjpeg({
-                quality: 0.5
+                quality: 50
             }),
             imageminPngquant({
                 quality: [0.5, 1]
             }),
-            imageminSvgo(),            
+            imageminSvgo(),
         ]
     })
-
-const storeImageLocally = async (filename, buffer) => {
-        const localFilepath = getLocalFilepath(filename);
-        const fd = fs.openSync(localFilepath, "w");
-        const bytesWritten = fs.writeSync(fd, buffer);
-        console.info(`Successfully wrote ${bytesWritten} to the local file system`);
-        return getRemoteFilepath(filename);
 }
 
-const getLocalFilepath = filename => {
+const storeImageLocally = async (filename, buffer) => {
+    const localFilepath = getLocalFilepath(filename);
+    const fd = fs.openSync(localFilepath, "w");
+    const bytesWritten = fs.writeSync(fd, buffer);
+    console.info(`Successfully wrote ${bytesWritten} to the local file system`);
+    return getRemoteFilepath(filename);
+}
+
+const getLocalFilepath = (filename) => {
     return path.resolve(__dirname, "..", "..", "public", "image-uploads", filename);
 }
 
 
-const getRemoteFilepath = filename => {
+const getRemoteFilepath = (filename) => {
     return `${process.env.PROTOCOL}://${process.env.DOMAIN}:${process.env.PORT}/image-uploads/${filename}`;
 }
 
-const storeImageRemotely = (filename, buffer) => {
+const storeImageRemotely = (filename, contentType, buffer) => {
     return new Promise((resolve, reject) => {
         const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
         const params = {
             Bucket: process.env.AWS_BUCKET_NAME,
             Key: `image-uploads/${filename}`,
             Body: buffer,
-            ContentType: "application/octet-stream"
+            ContentType: contentType
         }
         s3.upload(params, (err, data) => {
             if (err) reject(err);
